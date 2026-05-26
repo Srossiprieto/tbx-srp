109baeb662dc929196b9885aeb4d9fcabb72e3ff
srpmac <srossiprieto@gmail.com>
2026-05-25 22:58:55 -0300

    feat(frontend): Header, FileFilter and FilesTable components

---
+++ frontend/src/components/FileFilter.js
@@ -0,0 +1,15 @@
+import Form from 'react-bootstrap/Form'
+
+export default function FileFilter ({ files, value, onChange }) {
+  return (
+    <Form.Group className='mb-3' style={{ maxWidth: 320 }}>
+      <Form.Label>Filtrar por archivo</Form.Label>
+      <Form.Select value={value} onChange={(event) => onChange(event.target.value)}>
+        <option value=''>Todos los archivos</option>
+        {files.map((file) => (
+          <option key={file} value={file}>{file}</option>
+        ))}
+      </Form.Select>
+    </Form.Group>
+  )
+}
